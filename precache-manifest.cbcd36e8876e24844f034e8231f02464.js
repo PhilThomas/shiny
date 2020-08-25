4985016bbb1d02ff4c7d17504ad56590394c7650
Phil Thomas <crynoid2k7@gmail.com>
2020-08-25 10:45:07 -0700

    Updates

---
+++ precache-manifest.cbcd36e8876e24844f034e8231f02464.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c073c6769505777f686be02fab5fb6f7",
+    "url": "/shiny/index.html"
+  },
+  {
+    "revision": "7dd03884667f613635f9",
+    "url": "/shiny/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fe92ff9a97ad599a709f",
+    "url": "/shiny/static/js/2.1aa92d3b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shiny/static/js/2.1aa92d3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dd03884667f613635f9",
+    "url": "/shiny/static/js/main.370aec49.chunk.js"
+  },
+  {
+    "revision": "998cc64ce45014e26ab6",
+    "url": "/shiny/static/js/runtime-main.fb59f881.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/shiny/static/media/logo.5d5d9eef.svg"
+  }
+]);
